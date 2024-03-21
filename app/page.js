2a22107a2ba8bde11a5a3afd4859c7c7f23868d3
emeric mathis <emericmathis@gmail.com>
2024-03-21 12:54:38 +0100

    responsive top of the  home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import { HeroSection } from "./components/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-2 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }

a9309a62d91d764ac0d7bee0244fbc161e9bdd9f
emeric mathis <emericmathis@gmail.com>
2024-03-25 13:10:32 +0100

    save

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                 >
                     <div class="w-0 h-0 
   border-l-[50px] border-l-transparent
-  border-b-[75px] border-b-yellow-500
+  border-b-[75px] border-b-purple-500
   border-r-[50px] border-r-transparent">
                     </div>
                 </Link>

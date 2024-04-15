362cee7b24a1f4920ecd69edc3d68314556ccbbf
emeric mathis <emericmathis@gmail.com>
2024-04-15 12:07:14 +0200

    test email form resend

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -11,7 +11,7 @@ export async function POST(req, res) {
 
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email],
+      to: ["emericmathis@gmail.com"],
       subject: subject,
       react: (
         <>

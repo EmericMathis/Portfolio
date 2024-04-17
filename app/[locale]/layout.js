4a9a41b2c53f29bfebb2cb06bcc3cbdc873e27b1
emeric mathis <emericmathis@gmail.com>
2024-04-17 19:58:58 +0200

    save

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -7,7 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Emeric Mathis - Portfolio",
   description: "Web developer portfolio of Emeric Mathis.",
-  image: "/images/image.jpg",
+  images: {
+    url: 'https://nextjs.org/og.png',
+    alt: 'Next.js Logo',
+  },
   author: "Emeric Mathis",
   publishdate: "17-04-2024",
 };
@@ -21,12 +24,9 @@ export default function RootLayout({ children }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:alt" content={metadata.description} />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={metadata.title} />
         <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content={metadata.image} />
         <meta name="author" content={metadata.author} />
         <meta name="publishdate" content={metadata.publishdate} />
         <link rel="preload" as="image" href="/images/image.jpg"></link>

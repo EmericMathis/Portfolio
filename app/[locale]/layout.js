d01c28c058480a7563cd2c66998b72c506d1e6c2
emeric mathis <emericmathis@gmail.com>
2024-04-17 19:12:02 +0200

    save

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta property="twitter:image" content={metadata.image} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );

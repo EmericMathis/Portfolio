3ee88cbedb7a78890d5b50a5ec608a28abf382f8
emeric mathis <emericmathis@gmail.com>
2024-04-19 15:24:30 +0200

    metadata update

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Emeric Mathis - Portfolio",
   description: "Web developer portfolio of Emeric Mathis.",
-  images: { url: "https://www.emericmathis.com/images/image.png" },
+  image: "https://www.emericmathis.com/images/image.png",
+  url: "https://www.emericmathis.com"
 };
 
 export default function RootLayout({ children }) {
@@ -16,10 +17,18 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <meta itemprop="name" content={metadata.title} />
+        <meta itemprop="description" content={metadata.description} />
+        <meta itemprop="image" content={metadata.image} />
+        <meta property="og:url" content={metadata.url} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.images.url} />
+        <meta property="og:image" content={metadata.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>

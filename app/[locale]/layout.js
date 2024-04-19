82febf9cb6f9d936b272e9e7bb0ed9d5f8ff54c7
emeric mathis <emericmathis@gmail.com>
2024-04-19 15:26:08 +0200

    metadata update

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -7,29 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Emeric Mathis - Portfolio",
   description: "Web developer portfolio of Emeric Mathis.",
-  image: "https://www.emericmathis.com/images/image.png",
-  url: "https://www.emericmathis.com"
+  images: { url: "https://www.emericmathis.com/images/image.png" },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta itemprop="name" content={metadata.title} />
-        <meta itemprop="description" content={metadata.description} />
-        <meta itemprop="image" content={metadata.image} />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Head>
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:image" content={metadata.images.url} />
       <body className={inter.className}>{children}</body>
     </html>
   );

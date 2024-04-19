5065c46952dce4fecff498af7849ab405c33b298
emeric mathis <emericmathis@gmail.com>
2024-04-19 15:54:24 +0200

    form response update

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,15 +1,11 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-import { useTranslation } from "react-i18next";
-
-
+import i18next from "i18next";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-
-  const { t } = useTranslation('emailsection')
 
   try {
     const { email, subject, message } = await req.json();
@@ -21,8 +17,8 @@ export async function POST(req, res) {
       subject: subject,
       react: (
         <>
-          <p>{t('thank you')}</p>
-          <p>{t('submitted')}</p>
+          <p>{i18next.t('emailsection:thank you')}</p>
+          <p>{i18next.t('emailsection:submitted')}</p>
           <h1>{subject}</h1>
           <p>{message}</p>
         </>

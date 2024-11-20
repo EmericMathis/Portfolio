a0d3bddc6d14b6341051d541f362b58add90dc18
emeric mathis <137341067+EmericMathis@users.noreply.github.com>
2024-11-21 00:47:02 +0100

    metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
